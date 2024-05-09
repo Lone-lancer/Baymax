55a1a4746ff887a9466dbab0b578a8aa8a08b3ad
Arlott <abenezerxsisay@gmail.com>
2024-05-09 20:59:41 +0300

    updated with login and signup fixes

--- main.py
+++ main.py
@@ -85,6 +85,67 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
+@app.route("/kidney")
+@login_required
+def kidney():
+    return render_template("kidney.html")
+
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1, size)
+    if size == 10:
+        loaded_model = joblib.load('model/kidney_disease_model.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route("/predictkidney",  methods=['GET', 'POST'])
+def predictkidney():
+     # Default value for result
+    result = 0
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        if len(to_predict_list) == 10:
+            result = ValuePredictor(to_predict_list, 10)
+    if(int(result) == 1):
+        prediction = "Patient has a low risk of Kidney Disease"
+    else:
+        prediction = "Patient has a high risk of Kidney Disease, please consult your doctor immediately"
+    return render_template("kidney_result.html", prediction_text=prediction)
+
+@app.route("/cancer")
+@login_required
+def Cancer():
+    return render_template("cancer.html")
+
+def ValuePred(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==9):
+        loaded_model = joblib.load('model/Breast_Cancer_model.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/predictcancer', methods=["POST"])
+def predictcancer():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+
+     # Default value for result
+        result = 0
+
+        if len(to_predict_list) == 9:
+            result = ValuePred(to_predict_list, 9)
+
+    if int(result) == 1:
+        prediction = "Patient has a high risk of breast cancer Disease, please consult your doctor immediately"
+    else:
+        prediction = "Patient has a low risk of breast cancer"
+    return render_template("cancer_result.html", prediction_text=prediction)
+
 if __name__ == "__main__":
     
     with app.app_context():
